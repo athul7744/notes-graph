5a6f227b410daccf324f4bc772bb7209814cdbc9
athul7744 <athul7744@outlook.com>
2020-04-30 00:33:36 +0530

    Initial load file

---
+++ src/app/main.js
@@ -0,0 +1,16 @@
+const EditorJS = require('@editorjs/editorjs');
+const autosize = require('autosize'); 
+const showdown = require('showdown');
+const winston = require('winston');
+const DailyRotateFile = require('winston-daily-rotate-file');
+const Dexie = require('dexie');
+
+var logger = new SimpleLogger();
+var router = new SimpleRouter();
+var converter = new showdown.Converter();
+var fieldsInstance = new FieldHandler();
+var dbInstance = new DatabaseHandler();
+var editorInstance = new EditorHandler();
+
+editorInstance.create();
+
