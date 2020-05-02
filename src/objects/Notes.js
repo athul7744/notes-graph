7d60cbe899785ac0ea6ad02c6a495cf9da6cb54b
athul7744 <athul7744@outlook.com>
2020-05-03 01:03:04 +0530

    Added title field

--- src/objects/Notes.js
+++ src/objects/Notes.js
@@ -5,6 +5,7 @@ class Note {
         this.created_time = new Date().getTime();
         this.updated_time = null;
         this.linked_notes = [];
+        this.title = '';
     }
 
     load(obj) {
@@ -13,5 +14,6 @@ class Note {
         this.updated_time = obj.updated_time;
         this.id = obj.id;
         this.linked_notes = obj.linked_notes;
+        this.title = obj.data.blocks[0].data.text;
     }
 }
