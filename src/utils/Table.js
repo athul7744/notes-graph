e754333c05010184d1b4bd53b611b2716370894c
athul7744 <athul7744@outlook.com>
2020-05-07 00:16:24 +0530

    Added comments

--- src/utils/Table.js
+++ src/utils/Table.js
@@ -1,11 +1,19 @@
 class SimpleTable {
 
+    /**
+     * @constructor
+     * @param {Object} config Table Config 
+     */
     constructor(config) {
         this.holder = document.getElementById(config.holder);
         this.columns = (config.columns != undefined) ? config.columns : [];
         this.data = (config.data != undefined ) ? config.data : [];
     }
 
+    /**
+     * Generates table object from config
+     * @returns null
+     */
     generateTable() {
         this.table = document.createElement("table");
         this.renderRows();
@@ -13,6 +21,10 @@ class SimpleTable {
         this.holder.innerHTML = this.table.outerHTML;
     }
 
+    /**
+     * Render the table rows
+     * @returns null 
+     */
     renderRows() {
         for (var obj of this.data) {
             var row = this.table.insertRow();
@@ -24,6 +36,10 @@ class SimpleTable {
         }
     }
 
+    /**
+     * Render the table head
+     * @returns null
+     */
     renderTableHead() {
         var thead = this.table.createTHead();
         var th_row = thead.insertRow();
